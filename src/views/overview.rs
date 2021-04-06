3321adc199e694ae074659a6ec53560d9bee73c5
Max Mindlin <maxmindlin@gmail.com>
2021-04-06 13:44:48 -0400

    tweak

--- src/views/overview.rs
+++ src/views/overview.rs
@@ -15,30 +15,53 @@ use tui::{
 };
 
 const X_WINDOW: u64 = 100;
-const Y_PADDING: u64 = 10;
+const Y_PADDING: u64 = 5;
+
+#[derive(Default)]
+struct ChartData {
+    data: Vec<(f64, f64)>,
+    // For right now we are using
+    // a generic tick counter as our
+    // time tracking.
+    counter: f64,
+}
+
+impl ChartData {
+    fn push(&mut self, n: f64) {
+        self.data.push((self.counter.clone(), n));
+        self.counter += 1.0;
+        if self.data.len() > X_WINDOW as usize {
+            self.data.drain(..1);
+        }
+    }
+
+    fn x_max(&self) -> u64 {
+        self.data
+            .iter()
+            .map(|p| p.0 as u64)
+            .max()
+            .unwrap()
+    }
+
+    fn y_max(&self) -> u64 {
+        self.data
+            .iter()
+            .map(|p| p.1 as u64)
+            .max()
+            .unwrap()
+    }
+}
 
 #[derive(Default)]
 struct OverviewData {
-    overall: Vec<(f64, f64)>,
-    ready: Vec<(f64, f64)>,
-    unacked: Vec<(f64, f64)>,
-}
-
-impl OverviewData {
-    fn update(&mut self, update: Overview) {
-        self.overall.push(
-            (
-                self.overall.last().unwrap().0 + 1.0,
-                update.queue_totals.messages as f64,
-            )
-        );
-    }
+    overall: ChartData,
+    ready: ChartData,
+    unacked: ChartData,
 }
 
 pub struct OverviewPane<'a, M> where
     M: ManagementClient,
 {
-    dataset: Vec<(f64, f64)>,
     data: OverviewData,
     client: &'a M,
 }
@@ -50,13 +73,14 @@ impl<'a, M> Pane<OverviewPane<'a, M>> where
         M: ManagementClient,
     {
         let data = client.get_overview();
-        let dataset = vec![(0.0, data.queue_totals.messages)];
-        let overall = vec![(0.0, data.queue_totals.messages)];
-        let ready = vec![(0.0, data.queue_totals.messages_ready)];
-        let unacked = vec![(0.0, data.queue_totals.messages_unacked)];
+        let mut overall = ChartData::default();
+        overall.push(data.queue_totals.messages);
+        let mut ready = ChartData::default();
+        ready.push(data.queue_totals.messages_ready);
+        let mut unacked = ChartData::default();
+        unacked.push(data.queue_totals.messages_unacked);
         Self {
             content: OverviewPane {
-                dataset,
                 client,
                 data: OverviewData {
                     overall,
@@ -72,16 +96,17 @@ impl<'a, M> Drawable for OverviewPane<'a, M> where
     M: ManagementClient,
 {
     fn draw<B: Backend>(&mut self, f: &mut Frame<B>, area: Rect) {
-        let data_y_max = self.dataset
-            .iter()
-            .map(|p| p.1 as u64)
+        let data_y_maxes = vec![
+            self.data.overall.y_max(),
+            self.data.ready.y_max(),
+            self.data.unacked.y_max(),
+        ];
+        let data_y_max = data_y_maxes.iter()
             .max()
             .unwrap();
-        let data_x_max = self.dataset
-            .iter()
-            .map(|p| p.0 as u64)
-            .max()
-            .unwrap();
+        // let data_y_max = self.data.ready.y_max();
+        // all should have the same
+        let data_x_max = self.data.ready.x_max();
         let y_max = data_y_max + Y_PADDING;
         let chunks = Layout::default()
             .direction(Direction::Vertical)
@@ -95,10 +120,23 @@ impl<'a, M> Drawable for OverviewPane<'a, M> where
             .split(area);
         let datasets = vec![
             Dataset::default()
-                .name("Messages")
-                .marker(symbols::Marker::Braille)
+                .name("Ready")
+                .marker(symbols::Marker::Dot)
                 .style(Style::default().fg(Color::Yellow))
-                .data(self.dataset.as_slice()),
+                .graph_type(GraphType::Line)
+                .data(self.data.ready.data.as_slice()),
+            Dataset::default()
+                .name("Total")
+                .marker(symbols::Marker::Dot)
+                .style(Style::default().fg(Color::Cyan))
+                .graph_type(GraphType::Line)
+                .data(self.data.overall.data.as_slice()),
+            Dataset::default()
+                .name("Unacked")
+                .marker(symbols::Marker::Dot)
+                .style(Style::default().fg(Color::Red))
+                .graph_type(GraphType::Line)
+                .data(self.data.unacked.data.as_slice()),
         ];
         let lb = if data_x_max < X_WINDOW {
             0
@@ -130,6 +168,12 @@ impl<'a, M> Drawable for OverviewPane<'a, M> where
                         Span::styled(format!("{}", y_max), Style::default().add_modifier(Modifier::BOLD)),
                     ])
                     .bounds([0.0, y_max as f64]),
+            )
+            .hidden_legend_constraints(
+                (
+                    Constraint::Ratio(1, 3),
+                    Constraint::Ratio(3, 4)
+                )
             );
         f.render_widget(chart, chunks[0]);
 
@@ -141,12 +185,8 @@ impl<'a, M> Drawable for OverviewPane<'a, M> where
 
     fn update(&mut self) {
         let update = self.client.get_overview();
-        self.dataset.push(
-            (
-                self.dataset.last().unwrap().0 + 1.0,
-                self.dataset.last().unwrap().1 + 1.0,
-                // update.queue_totals.messages + 1.0,
-            )
-        );
+        self.data.ready.push(update.queue_totals.messages_ready + 1.0);
+        self.data.overall.push(update.queue_totals.messages + 2.0);
+        self.data.unacked.push(update.queue_totals.messages_unacked + 4.0);
     }
 }
